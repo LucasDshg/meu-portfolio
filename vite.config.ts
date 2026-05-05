afb02d7f28d93cb1de45141e0201d0dddf802bf9
Lucas Gomes <lucas_dshgomes@hotmail.com>
2026-05-04 23:15:55 -0300

    chore: config project

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,28 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import obfuscator from 'vite-plugin-javascript-obfuscator';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // eslint-disable-next-line no-undef
+    process.env.NODE_ENV === 'production'
+      ? obfuscator({
+          options: {
+            compact: true,
+            controlFlowFlattening: true,
+            deadCodeInjection: true,
+            stringArray: true,
+            stringArrayThreshold: 0.75,
+          },
+          exclude: [/node_modules/],
+        })
+      : null,
+  ].filter(Boolean),
   resolve: {
     alias: {},
+  },
+  build: {
+    sourcemap: false,
   },
 });
