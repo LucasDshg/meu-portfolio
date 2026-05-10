38ad36eff923d84636d497c63b95c4e9ee05f10e
Lucas Gomes <lucas_dshgomes@hotmail.com>
2026-05-10 12:12:20 -0300

    feat: heading color

--- src/Lib/Heading.tsx
+++ src/Lib/Heading.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
-export const Heading: React.FC<React.ComponentPropsWithoutRef<'h2'>> = ({
+interface IHeadingProps extends React.ComponentPropsWithoutRef<'h2'> {
+  color?: string;
+}
+
+export const Heading: React.FC<IHeadingProps> = ({
   className = '',
+  color = 'text-zinc-800 dark:text-zinc-100',
   ...props
 }) => {
   return (
     <h2
       data-slot="heading"
-      className={`text-2xl font-bold leading-10 sm:leading-16 tracking-tight text-zinc-800 dark:text-zinc-100 ${className}`}
+      className={`text-2xl font-bold leading-10 sm:leading-16 tracking-tight  ${className} ${color}`}
       {...props}
     />
   );
